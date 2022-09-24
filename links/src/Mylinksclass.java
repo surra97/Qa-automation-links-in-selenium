00517d6e502c5f78d88ebabb261c04f57e6ed372
surra97 <suraakhras@gmail.com>
2022-09-24 14:22:48 +0300

    ss

---
+++ links/src/Mylinksclass.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Mylinksclass {
+
+	public static void main(String[] args) {
+		// hello
+		System.setProperty("webdriver.chrome.driver", "D:\\New folder\\chromedriver.exe");
+
+//		WebDriver driver = new WebDriver();
+	}
+
+}
---
+++ selenuimlink/src/selenuimlink/Selink.java
@@ -0,0 +1,10 @@
+package selenuimlink;
+
+public class Selink {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
